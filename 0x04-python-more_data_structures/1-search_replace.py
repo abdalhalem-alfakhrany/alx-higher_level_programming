0b95bfce3427363da904887f16fe2f70e4ee41d4
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2023-11-10 21:17:32 +0200

    second task created

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,8 @@
+def search_replace(my_list, search, replace):
+    result = []
+    for i in range(len(my_list)):
+        if my_list[i] == search:
+            result.append(replace)
+        else:
+            result.append(my_list[i])
+    return result
