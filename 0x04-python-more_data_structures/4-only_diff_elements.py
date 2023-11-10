0300500207f7624c62b9d7a189289271174a7882
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2023-11-10 21:45:04 +0200

    fifth task created

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -6,13 +6,10 @@ def only_diff_elements(set_1, set_2):
         for s2 in set_2:
             if s1 == s2:
                 unqui.append(s1)
-    
     for s1 in set_1:
         if s1 not in unqui:
             result.append(s1)
-    
     for s2 in set_2:
         if s2 not in unqui:
             result.append(s2)
-
     return result
