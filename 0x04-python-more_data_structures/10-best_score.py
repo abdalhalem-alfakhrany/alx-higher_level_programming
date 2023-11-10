d5ce771fdaada7683a8059ab6a39848632ac80df
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2023-11-10 22:10:42 +0200

    eleventh task edited

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    max_idx = 0
-    max_value = 0
-    if a_dictionary == None:
-        return None
-    for i in a_dictionary.keys():
-        if a_dictionary[i] > max_value:
-            max_value = a_dictionary[i]
-            max_idx = i
-    return max_idx
+    return None if a_dictionary == None else max(a_dictionary,key=a_dictionary.get)
