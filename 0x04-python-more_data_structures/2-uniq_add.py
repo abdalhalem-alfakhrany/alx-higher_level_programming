443e484cee1e5d202f2f8b72d545d778f104f05b
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2023-11-10 21:35:14 +0200

    third task edited [the new line]

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,15 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    result = 0
-    uniq = {}
-
-    for i in my_list:
-        if uniq.get(i) != None:
-            uniq[i] = 1 + uniq.get(i)
-        else:
-            uniq[i] = 1
-
-    for i in uniq.keys():
-        result += i
-
-    return result
+    new = set(my_list)
+    res = 0
+    for i in new:
+        res += i
+    return res
