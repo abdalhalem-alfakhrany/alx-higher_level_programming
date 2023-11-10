950596f041dbc67d12c2579fb3f854d6f0574654
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2023-11-10 21:10:13 +0200

    firest task edited [betty]

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -5,5 +5,4 @@ def square_matrix_simple(matrix=[]):
         result.append([])
         for col in range(len(matrix[row])):
             result[row].append(matrix[row][col] ** 2)
-    
     return result
